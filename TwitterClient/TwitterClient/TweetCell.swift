f2db96ade92bd812966a708f78bf99c69fe5d748
Uttam Singh <uttam_singh@intuit.com>
2017-04-17 10:06:29 +0530

    Twitter Client

--- TwitterClient/TwitterClient/TweetCell.swift
+++ TwitterClient/TwitterClient/TweetCell.swift
@@ -7,21 +7,26 @@
 //
 import UIKit
 
-class tweetCell: UITableViewCell {
+class TweetCell: UITableViewCell {
   
-  @IBOutlet weak var profPicView: UIImageView!
-  @IBOutlet weak var profPicButton: UIButton!
-  @IBOutlet weak var usernameLabel: UILabel!
-  @IBOutlet weak var timestamp: UILabel!
-  @IBOutlet weak var tweetTextLabel: UILabel!
-  @IBOutlet weak var handleLabel: UILabel!
-  @IBOutlet weak var replyButton: UIButton!
-  @IBOutlet weak var rtButton: UIButton!
-  @IBOutlet weak var favButton: UIButton!
-  @IBOutlet weak var rtCountLabel: UILabel!
-  @IBOutlet weak var favCountLabel: UILabel!
+    @IBOutlet weak var profPicView: UIImageView!
+    @IBOutlet weak var profPicButton: UIButton!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var timestamp: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var handleLabel: UILabel!
+    
+    @IBOutlet weak var replyButton: UIButton!
+    @IBOutlet weak var rtButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
+
+    @IBOutlet weak var rtCountLabel: UILabel!
+    @IBOutlet weak var favCountLabel: UILabel!
   
-  
+    @IBOutlet weak var rtImage: UIImageView!
+    @IBOutlet weak var rtTextLabel: UILabel!
+    @IBOutlet weak var rtHolderView: UIView!
+    
   // Observer to update whenever Tweet changes
   var tweet: Tweet! {
     didSet{
@@ -30,11 +35,15 @@ class tweetCell: UITableViewCell {
       usernameLabel.text = tweet.user?.name!
       handleLabel.text = "@\((tweet.user?.screenname)!)"
       profPicView.setImageWith((tweet.user?.profileUrl)!)
-      //profPicButton.setImageFor(.normal, with: (tweet.user?.profileUrl)!)
-      
       rtCountLabel.text = tweet.rtCountStr
       favCountLabel.text = tweet.favCountStr
       
+        if tweet.rtCount > 0 {
+            rtHolderView.isHidden = false
+        } else {
+            rtHolderView.isHidden = true
+        }
+        
       if (!self.tweet.retweeted!) {
        rtButton.setImage(UIImage(named: "retweet-icon"), for: .normal)
       } else {
@@ -65,7 +74,19 @@ class tweetCell: UITableViewCell {
     rtCountLabel.text = self.tweet.rtCountStr
     
     // post to twitter
-    
+    if (self.tweet.retweeted!) {
+        TwitterClient.sharedInstance?.retweet(tweet: tweet, success: { (retweet) in
+            print("retweeted successfully")
+        }, failure: { (error) in
+           print("Error: \(error.localizedDescription)")
+        })
+    } else {
+        TwitterClient.sharedInstance?.unretweet(tweet: tweet, success: { (retweet) in
+            print("unretweeted successfully")
+        }, failure: { (error) in
+            print("Error: \(error.localizedDescription)")
+        })
+    }
   }
   
   
@@ -85,8 +106,24 @@ class tweetCell: UITableViewCell {
     favCountLabel.text = self.tweet.favCountStr
     
     // post to twitter
+   if (self.tweet.faved!) {
+        TwitterClient.sharedInstance?.favorite(tweet: tweet, success: { (tweet) in
+            print("favorite successfully")
+        }, failure: { (error) in
+            print("Error: \(error.localizedDescription)")
+        })
+   } else {
+        TwitterClient.sharedInstance?.unfavorite(tweet: tweet, success: { (tweet) in
+            print("unfavorite successfully")
+        }, failure: { (error) in
+            print("Error: \(error.localizedDescription)")
+        })
+    }
   }
   
+    @IBAction func onReplyTap(_ sender: Any) {
+        
+    }
   
   override func awakeFromNib() {
     super.awakeFromNib()
